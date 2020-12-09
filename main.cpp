bcfcc47618469b3935011e5b702ca42ef45cf9e3
TymoteuszJakubowski <62596368+TymoteuszJakubowski@users.noreply.github.com>
2020-12-09 15:07:24 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,93 +1,73 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <math.h>
+
 using namespace std;
 
-struct uczen
+void sito( int a, bool * tab )
 {
-        string imie, nazwisko;
-        int numer;
-};
 
-class klasa
+    for( int i = 2; i * i <= a; i++ )
+    if( tab[ i ] == 0 )
+    for( int b = i * i; b <= a; b += 2 )
+    {
+        tab[ b ] = 1;
+    }
+
+}
+bool trzyczyn( int liczba, bool * tab )
 {
-                vector <struct uczen> spis;
+    int druga[ 4 ] { }, i = 1, c = 0;
+    do
+    {
 
-        public:
-                void wprowadz();
-                void wypisz();
-                void zapiszDoPliku(const char *);
-                void wczytajZPliku(const char *);
-};
+        i++;
 
-void klasa::wprowadz()
-{
-        int iloscUczniow;
-        struct uczen wprowadzany;
-        cout << "Ilosc uczniow: ";
-        cin  >> iloscUczniow;
-        for (int i=0; i<iloscUczniow; i++)
+
+        if( tab[ i ] == 0 )
+        if( liczba % i == 0 )
         {
-                cout << "Nr Imie Nazwisko: ";
-                cin >> wprowadzany.numer
-                        >> wprowadzany.imie
-                        >> wprowadzany.nazwisko;
-                spis.push_back(wprowadzany); //TU WSTAW do tablicy nowy rekord
+            liczba /= i;
+            if( i % 2 == 0 )
+                 return false;
+
+            for( int y = 0; y <= 3; y++ )
+            if( druga[ y ] == 0 )
+            {
+                druga[ y ] = i;
+                c++;
+                break;
+
+            }
+            i = 2;
         }
+    } while( liczba != 1 );
+
+    if( c == 3 )
+         return true;
+    else
+         return false;
+
 }
 
-
-void klasa::wypisz()
+int main()
 {
-        for (vector<struct uczen>::iterator it = spis.begin(); it != spis.end(); ++it)
+
+    fstream plik, plik2;
+    bool * tab;
+    int liczba;
+    plik.open( "liczby.txt", ios::in );
+    plik2.open( "wyniki_liczby.txt", ios::out );
+    for( int i = 1; 1 <= 1000; i++ )
+    {
+        plik >> liczba;
+        tab = new bool[ liczba + 1 ] { };
+        sito( liczba, tab );
+        if( trzyczyn( liczba, tab ) )
         {
-                cout << it->numer << " "
-                         << it->imie << " "
-                         << it->nazwisko << "\n";
+            plik2 << liczba;
         }
-}
+        delete[] tab;
 
-
-
-void klasa::zapiszDoPliku(const char *nazwa)
-{
-        ofstream plik;
-        plik.open(nazwa);
-        for (vector<struct uczen>::iterator it = spis.begin(); it != spis.end(); ++it) // iterator to zmienna która przebiega ca³¹ tablicê
-        {
-                plik << it->numer << " "
-                         << it->imie << " "
-                         << it->nazwisko << "\n";
-        }
-        plik.close();
-}
-
-void klasa::wczytajZPliku(const char *nazwa)
-{
-		ifstream plik2;
-		string napis, imie, nazwisko;
-		int numer;
-		plik2.open("c:\\nazwa.txt", ios::in);
-		if(plik2.good())
-			while(!plik2.eof())
-			{
-				plik2>>imie;
-				plik2>>nazwisko;
-				plik2>>numer;
-				cout<<napis;
-				cout<<endl;
-			}
-			plik2.close();
-}
-
-
-
-int main(int argc, char** argv) {
-        klasa TI;
-        TI.wprowadz();
-        TI.wypisz();
-        TI.zapiszDoPliku("c:\\a.txt");
-	TI.wczytajZPliku("c:\\nazwa.txt");
-        return 0;
+    } return 0;
 }
